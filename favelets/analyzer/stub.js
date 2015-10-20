8ce5197f4cb0fd70eda5022490d87c009f78c3e9
Marko Martin <marcus@gratex.com>
2015-10-21 00:19:04 +0200

    [FIX] trackers, analytics, gemius added (bemius with hgmxy version)

--- favelets/analyzer/stub.js
+++ favelets/analyzer/stub.js
@@ -13,7 +13,7 @@
         // TODO: study more
         var markup =
             '<!DOCTYPE HTML>\
-				<html><head><title>Page Analyzer version 6.15.6</title> \
+				<html><head><title>Page Analyzer version 6.15.7</title> \
                 <link href="' + base + '/code2.css" rel="stylesheet"/>\
                 </head><body>\
                 <div class="progress">Loading scripts... please wait</div>\
